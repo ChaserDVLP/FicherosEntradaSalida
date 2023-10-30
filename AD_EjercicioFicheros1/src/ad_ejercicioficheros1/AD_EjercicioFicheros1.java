fa8887553b15992b255da92219697309dbc60626
xChaser <javier_sanchez_torres@hotmail.com>
2023-10-30 20:09:35 +0100

    Version inicial
    Esta es la versión inicial de este ejercicio

---
+++ AD_EjercicioFicheros1/src/ad_ejercicioficheros1/AD_EjercicioFicheros1.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ad_ejercicioficheros1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author xChas
+ */
+public class AD_EjercicioFicheros1 {
+
+    public static void main(String[] args) {
+
+        String linea;
+        String[] lista;
+        String resultado;
+        BufferedReader entrada = null;
+        BufferedWriter salida = null;
+        
+        try {
+            entrada = new BufferedReader(new FileReader("alumnosNotas.txt"));
+            salida = new BufferedWriter(new FileWriter("AlumnosMedias.txt"));
+            linea = entrada.readLine();
+            while (linea != null) {
+                int notaMedia = 0;
+                lista = linea.split(":");
+                
+                for (int i = 1; i < lista.length; i++) {
+                    int nota = Integer.parseInt(lista[i]);
+                    notaMedia += nota;
+                }
+                notaMedia /= 3;
+                resultado = "La nota de "+lista[0]+" es "+notaMedia+"\n";
+                System.out.println(resultado);
+                salida.write(resultado);
+                
+                linea = entrada.readLine();
+
+            }
+            
+            
+        } catch (IOException ex) {
+            System.out.println("No se ha encontrado el archivo");
+            
+        } finally {
+            
+            try {
+                if(entrada != null) {
+                entrada.close();
+            }
+                if(salida != null) {
+                    salida.close();
+                }
+                
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            
+        }
+        
+    }
+    
+}
